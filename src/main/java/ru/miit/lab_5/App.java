5f3801315f0fe1f27efbdd0a6157397d69f0db29
DrOne <atiholoz@stdev.ru>
2020-12-19 14:40:53 +0300

    release base POJO class Wagon

--- src/main/java/ru/miit/lab_5/App.java
+++ src/main/java/ru/miit/lab_5/App.java
@@ -3,5 +3,8 @@ package ru.miit.lab_5;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        String numsStr = "123";
+        System.out.println(numsStr);
+        System.out.println(Integer.parseInt(numsStr));
     }
 }
--- src/main/java/ru/miit/lab_5/Wagon.java
+++ src/main/java/ru/miit/lab_5/Wagon.java
@@ -1,4 +1,37 @@
 package ru.miit.lab_5;
 
 public class Wagon {
+    String esr;
+    Double conventionalLength;
+    Integer conventionalWagons;
+
+    public Wagon() {
+        esr = "";
+        conventionalLength = 0.0;
+        conventionalWagons = 0;
+    }
+
+    public String getEsr() {
+        return esr;
+    }
+
+    public Double getConventionalLength() {
+        return conventionalLength;
+    }
+
+    public Integer getConventionalWagons() {
+        return conventionalWagons;
+    }
+
+    public void setEsr(String esr) {
+        this.esr = esr;
+    }
+
+    public void setConventionalLength(Double conventionalLength) {
+        this.conventionalLength = conventionalLength;
+    }
+
+    public void setConventionalWagons(Integer conventionalWagons) {
+        this.conventionalWagons = conventionalWagons;
+    }
 }
